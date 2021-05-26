82464a144e6d6ad80ba1e714fd9ad1d6fc7198e7
Paulo-oRicardo <paulo.ricardo.prym@gmail.com>
2021-05-26 00:03:01 -0300

    Configura database postgres

---
+++ ormconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  migrations: ["./dist/database/migrations/**.js"],
+  entities: ["./dist/models/**.js"],
+  cli: {
+    migrationsDir: "./src/database/migrations",
+  },
+  entitiesDir: "src/models",
+};
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import express from "express";
 import "./database";
-import {routes} from "./routes";
+import { routes } from "./routes";
 import cors from "cors";
-
+import dotenv from "dotenv";
+dotenv.config();
 const port = 8080;
 export const app = express();
 app.use(cors());
 
 app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
-app.listen(process.env.PORT||port, () => {
- console.log(`Server running on localhost:${port}`);
-})
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server running on localhost:${port}`);
+});
